4fb1159878e3010c3a9ed5b154a7f637820cbc80
Hiago <hiagobica@gmail.com>
2026-07-13 17:21:07 -0300

    criando types.h/implemntacao de tipos e bus.h/implementacao do barramento

---
+++ build/CMakeFiles/emu.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/emu.dir/link.d"
+  "CMakeFiles/emu.dir/main.cpp.o"
+  "CMakeFiles/emu.dir/main.cpp.o.d"
+  "emu"
+  "emu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/emu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
